42e594a18d5d73e2ea41c530e69977b26dd542fc
Luis Rodriguez <luis@invokeia.com>
2025-09-03 15:15:39 +0000

    vuelve a analizar las modificaciones realizadas, el componente que muest

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -96,7 +96,7 @@ export function DataTable<TData, TValue>({
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
-    manualPagination: manualPagination,
+    manualPagination: isControlledPagination,
     ...(isControlledPagination && { onPaginationChange: onPaginationChange }),
   });
   
