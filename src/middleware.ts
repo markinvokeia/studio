ff5d47126ff15291e2aee9d81af3cc6eb4c9a818
Mark Molina <mark@invokeia.com>
2025-09-11 19:50:37 +0000

    Por defecto en el path / deberia redireccionarse a /es y cargar es como

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,13 @@ import createMiddleware from 'next-intl/middleware';
 import { locales } from './i18n';
 
 export default createMiddleware({
+  // A list of all locales that are supported
   locales,
+
+  // Used when no locale matches
   defaultLocale: 'es',
+
+  // Always show the locale in the URL
   localePrefix: 'always'
 });
 
