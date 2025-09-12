ba951234de3d666ac6365cfac67da1b970cd5182
Mark Molina <mark@invokeia.com>
2025-09-12 15:23:45 +0000

    Tienes que entender que el nombre de las imagenes sigue el siguiente pat

--- src/components/odontogram/index.tsx
+++ src/components/odontogram/index.tsx
@@ -36,7 +36,7 @@ class Engine {
         this.child_dientes_down = [];
 
         this.data_paciente = [];
-        for (var i = 1; i < 53; i++) {
+        for (var i = 1; i < 86; i++) {
             this.data_paciente[i] = [];
             this.data_paciente[i]['completo'] = "#80FFFF";
             for (var j = 1; j < 7; j++) {
@@ -125,18 +125,23 @@ class Engine {
         const path = '/odontograma/';
         const imagePromises: Promise<any>[] = [];
         
-        for (var i = 1; i < 6; i++) {
-            imagePromises.push(loadImage(path + 'numero_' + i + '.png').then(img => this.properties.images.numeros[i] = img));
+        const adult_teeth_sup = [18, 17, 16, 15, 14, 13, 12, 11, 21, 22, 23, 24, 25, 26, 27, 28];
+        const adult_teeth_inf = [48, 47, 46, 45, 44, 43, 42, 41, 31, 32, 33, 34, 35, 36, 37, 38];
+        const child_teeth_sup = [55, 54, 53, 52, 51, 61, 62, 63, 64, 65];
+        const child_teeth_inf = [85, 84, 83, 82, 81, 71, 72, 73, 74, 75];
+
+        for (const toothNum of adult_teeth_sup) {
+            imagePromises.push(loadImage(`${path}dentadura-sup-${toothNum}.png`).then(img => this.properties.images.dientes[toothNum] = img));
         }
-        for (var i = 1; i < 9; i++) {
-            imagePromises.push(loadImage(path + 'adult_diente_' + i + '.png').then(img => this.properties.images.dientes[i] = img));
+        for (const toothNum of adult_teeth_inf) {
+             imagePromises.push(loadImage(`${path}dentadura-inf-${toothNum}.png`).then(img => this.properties.images.dientes[toothNum] = img));
         }
-        for (var i = 1; i < 6; i++) {
-            imagePromises.push(loadImage(path + 'child_diente_' + i + '.png').then(img => this.properties.images.dientes[i + 10] = img));
+        for (const toothNum of child_teeth_sup) {
+             imagePromises.push(loadImage(`${path}dentadura-sup-${toothNum}.png`).then(img => this.properties.images.dientes[toothNum] = img));
         }
-        imagePromises.push(loadImage(path + 'marca_1.png').then(img => this.properties.images.marcas[1] = img));
-        imagePromises.push(loadImage(path + 'marca_2.png').then(img => this.properties.images.marcas[2] = img));
-        imagePromises.push(loadImage(path + 'marca_3.png').then(img => this.properties.images.marcas[3] = img));
+        for (const toothNum of child_teeth_inf) {
+             imagePromises.push(loadImage(`${path}dentadura-inf-${toothNum}.png`).then(img => this.properties.images.dientes[toothNum] = img));
+        }
 
         await Promise.all(imagePromises);
     }
@@ -148,18 +153,16 @@ class Engine {
             y: any;
             parts: any;
             image_diente: any;
-            image_numero: any;
 
-            constructor(id: any, x: any, y: any, parts: any, image_diente: any, image_numero: any) {
+            constructor(id: any, x: any, y: any, parts: any, image_diente: any) {
                 this.id = id;
                 this.x = x;
                 this.y = y;
                 this.parts = parts; // Polygon parts for hit detection
                 this.image_diente = image_diente;
-                this.image_numero = image_numero;
             }
 
-            draw(ctx: { drawImage: (arg0: any, arg1: any, arg2: any) => void; lineWidth: number; strokeStyle: string; fillStyle: any; beginPath: () => void; moveTo: (arg0: any, arg1: any) => void; lineTo: (arg0: any, arg1: any) => void; closePath: () => void; fill: () => void; stroke: () => void; strokeRect: (arg0: any, arg1: any, arg2: any, arg3: any) => void; }, diente_sel: number, parte_sel: number, data: { [x: string]: { [x: string]: any; }; }) {
+            draw(ctx: { drawImage: (arg0: any, arg1: any, arg2: any) => void; lineWidth: number; strokeStyle: string; fillStyle: any; beginPath: () => void; moveTo: (arg0: any, arg1: any) => void; lineTo: (arg0: any, arg1: any) => void; closePath: () => void; fill: () => void; stroke: () => void; strokeRect: (arg0: any, arg1: any, arg2: number, arg3: number) => void; }, diente_sel: number, parte_sel: number, data: { [x: string]: { [x: string]: any; }; }) {
                 if (this.image_diente) {
                    try {
                         ctx.drawImage(this.image_diente, this.x, this.y);
@@ -170,22 +173,24 @@ class Engine {
                 
                 ctx.lineWidth = 1;
                 ctx.strokeStyle = 'black';
-                for (var i = 0; i < this.parts.length; i++) {
-                    ctx.fillStyle = data[this.id][i+1];
-                    ctx.beginPath();
-                    ctx.moveTo(this.parts[i][0][0] + this.x, this.parts[i][0][1] + this.y);
-                    for(var j=1; j<this.parts[i].length; j++){
-                        ctx.lineTo(this.parts[i][j][0] + this.x, this.parts[i][j][1] + this.y);
+                if(this.parts) {
+                    for (var i = 0; i < this.parts.length; i++) {
+                        ctx.fillStyle = data[this.id][i+1];
+                        ctx.beginPath();
+                        ctx.moveTo(this.parts[i][0][0] + this.x, this.parts[i][0][1] + this.y);
+                        for(var j=1; j<this.parts[i].length; j++){
+                            ctx.lineTo(this.parts[i][j][0] + this.x, this.parts[i][j][1] + this.y);
+                        }
+                        ctx.closePath();
+                        ctx.fill();
+                        ctx.stroke();
                     }
-                    ctx.closePath();
-                    ctx.fill();
-                    ctx.stroke();
                 }
 
                 if (diente_sel == this.id) {
                     ctx.lineWidth = 2;
                     ctx.strokeStyle = '#FF0000'; // Red selection
-                    if (parte_sel > 0) {
+                    if (parte_sel > 0 && this.parts) {
                          const part = this.parts[parte_sel-1];
                          ctx.beginPath();
                          ctx.moveTo(part[0][0] + this.x, part[0][1] + this.y);
@@ -194,7 +199,7 @@ class Engine {
                          }
                          ctx.closePath();
                          ctx.stroke();
-                    } else {
+                    } else if (this.image_diente) {
                         ctx.strokeRect(this.x, this.y, this.image_diente.width, this.image_diente.height);
                     }
                 }
@@ -202,12 +207,14 @@ class Engine {
 
              isClicked(x: number, y: number) {
                 if (this.image_diente && x > this.x && x < this.x + this.image_diente.width && y > this.y && y < this.y + this.image_diente.height) {
-                    for (var i = 0; i < this.parts.length; i++) {
-                        if (isPointInPoly(this.parts[i], x - this.x, y - this.y)) {
-                            return {
-                                "diente": this.id,
-                                "parte": i + 1
-                            };
+                    if(this.parts) {
+                        for (var i = 0; i < this.parts.length; i++) {
+                            if (isPointInPoly(this.parts[i], x - this.x, y - this.y)) {
+                                return {
+                                    "diente": this.id,
+                                    "parte": i + 1
+                                };
+                            }
                         }
                     }
                     return {
@@ -222,47 +229,92 @@ class Engine {
         var x_off = 325;
         var y_off = 170;
         var separacion = 4;
+
+        const adult_teeth_sup_right = [18, 17, 16, 15, 14, 13, 12, 11];
+        const adult_teeth_sup_left = [21, 22, 23, 24, 25, 26, 27, 28];
+        const adult_teeth_inf_left = [31, 32, 33, 34, 35, 36, 37, 38];
+        const adult_teeth_inf_right = [41, 42, 43, 44, 45, 46, 47, 48];
         
-        for (var i = 0; i < 8; i++) {
-            var diente_info = this.parts[8 - (i + 1)];
-            var diente_img = this.properties.images.dientes[8 - i];
-            this.adult_dientes_up[i] = new Diente(18 - i, x_off - diente_img.width, y_off, diente_info, diente_img, this.properties.images.numeros[1]);
-            x_off -= (diente_img.width + separacion);
+        for (var i = 0; i < adult_teeth_sup_right.length; i++) {
+            const toothNum = adult_teeth_sup_right[i];
+            var diente_info = this.parts[18 - toothNum] || this.parts[0];
+            var diente_img = this.properties.images.dientes[toothNum];
+            if(diente_img) {
+                this.adult_dientes_up[i] = new Diente(toothNum, x_off - diente_img.width, y_off, diente_info, diente_img);
+                x_off -= (diente_img.width + separacion);
+            }
         }
         x_off = 325;
-        for (var i = 0; i < 8; i++) {
-            var diente_info = this.parts[i];
-            var diente_img = this.properties.images.dientes[i + 1];
-            this.adult_dientes_up[i + 8] = new Diente(21 + i, x_off, y_off, diente_info, diente_img, this.properties.images.numeros[2]);
-            x_off += (diente_img.width + separacion);
+        for (var i = 0; i < adult_teeth_sup_left.length; i++) {
+            const toothNum = adult_teeth_sup_left[i];
+            var diente_info = this.parts[toothNum - 21] || this.parts[0];
+            var diente_img = this.properties.images.dientes[toothNum];
+            if(diente_img) {
+                this.adult_dientes_up[i + 8] = new Diente(toothNum, x_off, y_off, diente_info, diente_img);
+                x_off += (diente_img.width + separacion);
+            }
         }
 
         x_off = 325;
         y_off = 430;
-        for (var i = 0; i < 8; i++) {
-            var diente_info = this.parts[i + 8];
-            var diente_img = this.properties.images.dientes[i + 1];
-            this.adult_dientes_down[i] = new Diente(31 + i, x_off, y_off, diente_info, diente_img, this.properties.images.numeros[3]);
-            x_off += (diente_img.width + separacion);
+        for (var i = 0; i < adult_teeth_inf_left.length; i++) {
+            const toothNum = adult_teeth_inf_left[i];
+            var diente_info = this.parts[toothNum - 31 + 8] || this.parts[0];
+            var diente_img = this.properties.images.dientes[toothNum];
+            if(diente_img) {
+                this.adult_dientes_down[i] = new Diente(toothNum, x_off, y_off, diente_info, diente_img);
+                x_off += (diente_img.width + separacion);
+            }
         }
+
         x_off = 325;
-        for (var i = 0; i < 8; i++) {
-            var diente_info = this.parts[15 - i];
-            var diente_img = this.properties.images.dientes[8 - i];
-            this.adult_dientes_down[i + 8] = new Diente(48 - i, x_off - diente_img.width, y_off, diente_info, diente_img, this.properties.images.numeros[4]);
-            x_off -= (diente_img.width + separacion);
+        const adult_teeth_inf_right_rev = [...adult_teeth_inf_right].reverse();
+         for (var i = 0; i < adult_teeth_inf_right_rev.length; i++) {
+            const toothNum = adult_teeth_inf_right_rev[i];
+            var diente_info = this.parts[48 - toothNum + 8] || this.parts[0];
+            var diente_img = this.properties.images.dientes[toothNum];
+             if(diente_img) {
+                this.adult_dientes_down[i + 8] = new Diente(toothNum, x_off - diente_img.width, y_off, diente_info, diente_img);
+                x_off -= (diente_img.width + separacion);
+             }
         }
     }
 
     loadParts() {
-        this.parts[0] = new Array(Array(3, 10), Array(13, 12), Array(13, 21), Array(3, 22)); //vestibular
-        this.parts[1] = new Array(Array(15, 12), Array(25, 10), Array(25, 22), Array(15, 21)); //distal
-        this.parts[2] = new Array(Array(3, 24), Array(13, 23), Array(13, 31), Array(3, 32)); //palatino
-        this.parts[3] = new Array(Array(3, 10), Array(13, 12), Array(13, 21), Array(3, 22)); //mesial..
-        this.parts[4] = new Array(Array(5, 14), Array(12, 13), Array(12, 22), Array(5, 21)); //oclusal..
-        for(let i=5; i<53*5; i++) { // Mocking the rest
-            this.parts[i] = this.parts[0];
-        }
+        this.parts = [
+            // Central superior
+            [ [[2,9],[12,11],[12,20],[2,21]], [[14,11],[24,9],[24,21],[14,20]], [[2,23],[12,22],[12,30],[2,31]], [[2,9],[12,11],[12,20],[2,21]], [[4,13],[12,12],[12,21],[4,20]] ],
+            // Lateral superior
+            [ [[3,10],[11,12],[11,20],[3,21]], [[13,12],[21,10],[21,21],[13,20]], [[3,23],[11,22],[11,30],[3,30]], [[3,10],[11,12],[11,20],[3,21]], [[5,13],[11,12],[11,21],[5,20]] ],
+            // Canino superior
+            [ [[2,10],[12,12],[12,20],[2,22]], [[14,12],[24,10],[24,22],[14,20]], [[2,24],[12,22],[12,32],[2,32]], [[2,10],[12,12],[12,20],[2,22]], [[4,13],[12,12],[12,21],[4,20]] ],
+            // 1er premolar superior
+            [ [[3,11],[12,12],[12,19],[3,20]], [[14,12],[23,11],[23,20],[14,19]], [[3,22],[12,21],[12,29],[3,28]], [[3,11],[12,12],[12,19],[3,20]], [[5,14],[12,13],[12,20],[5,19]] ],
+            // 2do premolar superior
+            [ [[3,11],[12,12],[12,19],[3,20]], [[14,12],[23,11],[23,20],[14,19]], [[3,22],[12,21],[12,29],[3,28]], [[3,11],[12,12],[12,19],[3,20]], [[5,14],[12,13],[12,20],[5,19]] ],
+            // 1er molar superior
+            [ [[4,10],[14,11],[14,19],[4,20]], [[16,11],[28,10],[28,20],[16,19]], [[4,22],[14,21],[14,29],[4,28]], [[4,10],[14,11],[14,19],[4,20]], [[6,13],[14,12],[14,20],[6,19]] ],
+            // 2do molar superior
+            [ [[4,10],[14,11],[14,19],[4,20]], [[16,11],[28,10],[28,20],[16,19]], [[4,22],[14,21],[14,29],[4,28]], [[4,10],[14,11],[14,19],[4,20]], [[6,13],[14,12],[14,20],[6,19]] ],
+            // 3er molar superior
+            [ [[4,10],[14,11],[14,19],[4,20]], [[16,11],[28,10],[28,20],[16,19]], [[4,22],[14,21],[14,29],[4,28]], [[4,10],[14,11],[14,19],[4,20]], [[6,13],[14,12],[14,20],[6,19]] ],
+            // Central inferior
+            [ [[3,11],[13,10],[13,22],[3,24]], [[15,10],[25,11],[25,24],[15,22]], [[3,26],[13,25],[13,32],[3,32]], [[3,11],[13,10],[13,22],[3,24]], [[5,12],[13,11],[13,23],[5,24]] ],
+            // Lateral inferior
+            [ [[3,11],[13,10],[13,22],[3,24]], [[15,10],[25,11],[25,24],[15,22]], [[3,26],[13,25],[13,32],[3,32]], [[3,11],[13,10],[13,22],[3,24]], [[5,12],[13,11],[13,23],[5,24]] ],
+            // Canino inferior
+            [ [[3,11],[13,10],[13,22],[3,24]], [[15,10],[25,11],[25,24],[15,22]], [[3,26],[13,25],[13,32],[3,32]], [[3,11],[13,10],[13,22],[3,24]], [[5,12],[13,11],[13,23],[5,24]] ],
+            // 1er premolar inferior
+            [ [[4,11],[14,10],[14,20],[4,21]], [[16,10],[26,11],[26,21],[16,20]], [[4,23],[14,22],[14,30],[4,30]], [[4,11],[14,10],[14,20],[4,21]], [[6,12],[14,11],[14,21],[6,20]] ],
+            // 2do premolar inferior
+            [ [[4,11],[14,10],[14,20],[4,21]], [[16,10],[26,11],[26,21],[16,20]], [[4,23],[14,22],[14,30],[4,30]], [[4,11],[14,10],[14,20],[4,21]], [[6,12],[14,11],[14,21],[6,20]] ],
+            // 1er molar inferior
+            [ [[5,10],[15,10],[15,20],[5,20]], [[17,10],[29,10],[29,20],[17,20]], [[5,22],[15,22],[15,30],[5,30]], [[5,10],[15,10],[15,20],[5,20]], [[7,12],[15,12],[15,20],[7,20]] ],
+            // 2do molar inferior
+            [ [[5,10],[15,10],[15,20],[5,20]], [[17,10],[29,10],[29,20],[17,20]], [[5,22],[15,22],[15,30],[5,30]], [[5,10],[15,10],[15,20],[5,20]], [[7,12],[15,12],[15,20],[7,20]] ],
+            // 3er molar inferior
+            [ [[5,10],[15,10],[15,20],[5,20]], [[17,10],[29,10],[29,20],[17,20]], [[5,22],[15,22],[15,30],[5,30]], [[5,10],[15,10],[15,20],[5,20]], [[7,12],[15,12],[15,20],[7,20]] ],
+        ];
     }
 
     drawPaleta() {
@@ -305,8 +357,10 @@ class Engine {
 
         for (var j = 0; j < collection.length; j++) {
             for (var i = 0; i < collection[j].length; i++) {
-                result = collection[j][i].isClicked(x, y);
-                if (result) break;
+                if(collection[j][i]) {
+                    result = collection[j][i].isClicked(x, y);
+                    if (result) break;
+                }
             }
             if (result) break;
         }
@@ -334,8 +388,6 @@ class Engine {
                  }
             }
         }
-        
-        this.draw();
     }
 
     onMouseMove(event: MouseEvent) {}
@@ -343,6 +395,7 @@ class Engine {
 }
 
 function isPointInPoly(poly: string | any[], x: number, y: number) {
+    if(!poly) return false;
     var i, j, c = false;
     for (i = 0, j = poly.length - 1; i < poly.length; j = i++) {
         if (((poly[i][1] > y) != (poly[j][1] > y)) && (x < (poly[j][0] - poly[i][0]) * (y - poly[i][1]) / (poly[j][1] - poly[i][1]) + poly[i][0])) {
@@ -380,6 +433,8 @@ export const OdontogramComponent = () => {
                     );
     
                     const canvas = canvasRef.current;
+                    if(!canvas) return;
+
                     const handleClick = (event: MouseEvent) => engine.onMouseClick(event);
                     const handleMouseMove = (event: MouseEvent) => engine.onMouseMove(event);
                     const handleKeyDown = (event: KeyboardEvent) => engine.onButtonClick(event);
@@ -390,8 +445,10 @@ export const OdontogramComponent = () => {
     
                     return () => {
                         if (cleanup) cleanup();
-                        canvas.removeEventListener('mousedown', handleClick);
-                        canvas.removeEventListener('mousemove', handleMouseMove);
+                        if (canvas) {
+                            canvas.removeEventListener('mousedown', handleClick);
+                            canvas.removeEventListener('mousemove', handleMouseMove);
+                        }
                         window.removeEventListener('keydown', handleKeyDown);
                     };
                 } catch (error) {
