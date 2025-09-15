6449ff4aa189c38c3a8a3be45c9437bf700dda47
Mark Molina <mark@invokeia.com>
2025-09-15 11:26:41 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
+
 import type {NextConfig} from 'next';
-const withNextIntl = require('next-intl/plugin')();
+const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
 
 const nextConfig: NextConfig = {
   /* config options here */
