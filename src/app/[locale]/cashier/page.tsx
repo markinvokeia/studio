e4a4abf888c34f6d873e005382545046837f3d6f
Luis Rodriguez <luis@invokeia.com>
2025-12-03 20:48:37 +0000

    Fix USD images

--- src/app/[locale]/cashier/page.tsx
+++ src/app/[locale]/cashier/page.tsx
@@ -51,12 +51,12 @@ const UYU_IMAGES: Record<number, string> = {
 };
 
 const USD_IMAGES: Record<number, string> = {
-    100: 'https://6000-firebase-studio-1756657738244.cluster-j6d3cbsvdbe5uxnhqrfzzeyj7i.cloudworkstations.dev/billetes/usd/USD_billete_100.svg',
-    50: 'https://6000-firebase-studio-1756657738244.cluster-j6d3cbsvdbe5uxnhqrfzzeyj7i.cloudworkstations.dev/billetes/usd/USD_billete_50.svg',
-    20: 'https://6000-firebase-studio-1756657738244.cluster-j6d3cbsvdbe5uxnhqrfzzeyj7i.cloudworkstations.dev/billetes/usd/USD_billete_20.svg',
-    10: 'https://6000-firebase-studio-1756657738244.cluster-j6d3cbsvdbe5uxnhqrfzzeyj7i.cloudworkstations.dev/billetes/usd/USD_billete_10.svg',
-    5: 'https://6000-firebase-studio-1756657738244.cluster-j6d3cbsvdbe5uxnhqrfzzeyj7i.cloudworkstations.dev/billetes/usd/USD_billete_5.svg',
-    1: 'https://6000-firebase-studio-1756657738244.cluster-j6d3cbsvdbe5uxnhqrfzzeyj7i.cloudworkstations.dev/billetes/usd/USD_billete_1.svg',
+    100: '/billetes/usd/USD_billete_100.svg',
+    50: '/billetes/usd/USD_billete_50.svg',
+    20: '/billetes/usd/USD_billete_20.svg',
+    10: '/billetes/usd/USD_billete_10.svg',
+    5: '/billetes/usd/USD_billete_5.svg',
+    1: '/billetes/usd/USD_billete_1.svg',
 };
 
 
