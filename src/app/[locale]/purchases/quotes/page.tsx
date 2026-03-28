e91b974aa3da42f6d760c45ae7da31fd59e2d19a
Raidel Martínez Santos <yoshilol0526@gmail.com>
2026-03-28 19:10:10 -0300

    fix: Update quote not saving updated services

--- src/app/[locale]/purchases/quotes/page.tsx
+++ src/app/[locale]/purchases/quotes/page.tsx
@@ -1295,7 +1295,7 @@ function QuotesPageContent() {
                     setEditingQuote(null);
                 }
             }}>
-                <DialogContent className="max-w-5xl max-h-[90vh] overflow-y-auto">
+                <DialogContent showMaximize={true} maxWidth="6xl">
                     <DialogHeader>
                         <DialogTitle>{editingQuote ? t('quoteDialog.editTitle') : t('quoteDialog.createTitle')}</DialogTitle>
                         <DialogDescription>
@@ -1563,7 +1563,7 @@ function QuotesPageContent() {
 
             {/* Quote Item Dialog */}
             <Dialog open={isQuoteItemDialogOpen} onOpenChange={setIsQuoteItemDialogOpen}>
-                <DialogContent className="max-w-5xl max-h-[90vh] overflow-y-auto">
+                <DialogContent className="max-w-5xl max-h-[95vh] overflow-y-auto">
                     <DialogHeader>
                         <DialogTitle>{editingQuoteItem ? t('itemDialog.editTitle') : t('itemDialog.createTitle')}</DialogTitle>
                         <DialogDescription>{t('itemDialog.description')}</DialogDescription>
